d83e7c74f109688c25b4438dd0bce7128008f01b
WangZT01 <18640096737@163.com>
2021-08-04 15:45:34 -0400

    project v1.0

---
+++ src/Server/MethodImplOperation.java
@@ -0,0 +1,111 @@
+package Server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.rmi.RemoteException;
+
+public class MethodImplOperation extends MethodImpl {
+
+
+    public MethodImplOperation() throws RemoteException {
+        super();
+    }
+
+    public String operating(String message){
+        if (!message.equals("")) {
+            byte[] reply = null;
+            String[] strings = message.split(",");
+            int messageId = Integer.parseInt(strings[0]);
+
+
+            String result = null;
+            switch (strings[1]) {
+                case "1":
+                    boolean flag = this.createTRecord(strings[2], strings[3], strings[4], strings[5], strings[6], strings[7], strings[8]);
+                    result = castBoolean2Return(flag);
+                    break;
+                case "2":
+                    flag = this.createSRecord(strings[2], strings[3], strings[4], strings[5], strings[6], strings[7]);
+                    result = castBoolean2Return(flag);
+                    break;
+                case "3":
+                        result = this.getRecordCounts();
+                        break;
+                case "4":
+                    flag = this.editRecord(strings[2], strings[3], strings[4], strings[5]);
+                    result = castBoolean2Return(flag);
+                    break;
+                case "5":
+                    flag = this.transferRecord(strings[2], strings[3], strings[4]);
+                    result = castBoolean2Return(flag);
+                    break;
+                case "6":
+                    System.out.println(strings[2]);
+                    flag = this.printRecord(strings[2]);
+                    result = castBoolean2Return(flag);
+                    break;
+                default:
+                    System.out.println("Server - receive invalid message");
+            }
+            //results.add(result);
+            return result;
+        }
+        return "UDP ERROR ";
+    }
+
+    private String castBoolean2Return(boolean b){
+        if(b)
+            return "SUCCESS";
+        else
+            return "Fail";
+    }
+
+    public void UDPServer(int port) {
+
+        try {
+            DatagramSocket server = null;
+            server = new DatagramSocket(port);
+            byte[] recvBuf = new byte[1000];
+            String sendStr = "5053 HELLO";
+
+            while (true) {
+                DatagramPacket recvPacket = new DatagramPacket(recvBuf, recvBuf.length);
+                server.receive(recvPacket);
+                String recvStr = new String(recvPacket.getData(), 0, recvPacket.getLength());
+                System.out.println(recvStr);
+                int Sendport = recvPacket.getPort();
+
+
+                /**
+                if (recvStr != null && recvStr.startsWith("getCount")) {
+
+                    sendStr = getCountForUDP(recvStr);
+
+                } else if (recvStr != null && recvStr.startsWith("Transfer")) {
+                    boolean result = transferForUDP(recvStr);
+                    if(result){
+                        sendStr = "Transfer success";
+                    }else{
+                        sendStr = "Transfer fail";
+                    }
+                }
+                 **/
+                sendStr = operating(recvStr);
+                InetAddress addr = recvPacket.getAddress();
+
+                byte[] sendBuf = sendStr.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendBuf, sendBuf.length, addr, Sendport);
+                server.send(sendPacket);
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
